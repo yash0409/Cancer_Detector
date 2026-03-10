ac501ac8a355d47bdcb3d9e706c5b7b5a93f3572
yash0409 <71770391+yash0409@users.noreply.github.com>
2026-03-10 19:26:26 +0530

    Switch from TensorFlow to TFLite runtime for model

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 
 # Class labels
 classes = ["lung_aca", "lung_n", "lung_scc"]
@@ -9,7 +9,7 @@ classes = ["lung_aca", "lung_n", "lung_scc"]
 # Load TFLite model
 @st.cache_resource
 def load_model():
-    interpreter = tf.lite.Interpreter(model_path="lung_cancer_classifier.tflite")
+    interpreter = tflite.Interpreter(model_path="lung_cancer_classifier.tflite")
     interpreter.allocate_tensors()
     return interpreter
 
@@ -20,7 +20,7 @@ output_details = interpreter.get_output_details()
 
 st.title("Lung Cancer Detection from Histopathology Images")
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg","jpeg","png"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
 
@@ -28,18 +28,18 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess image
-    img = image.resize((224,224))
+    img = image.resize((224, 224))
     img = np.array(img).astype(np.float32)
-    img = img / 255.0
+
+    # If you trained with ResNet preprocessing
+    img = (img / 127.5) - 1
+
     img = np.expand_dims(img, axis=0)
 
-    # Set input tensor
-    interpreter.set_tensor(input_details[0]['index'], img)
-
     # Run inference
+    interpreter.set_tensor(input_details[0]['index'], img)
     interpreter.invoke()
 
-    # Get prediction
     prediction = interpreter.get_tensor(output_details[0]['index'])
 
     predicted_class = classes[np.argmax(prediction)]
@@ -48,6 +48,6 @@ if uploaded_file is not None:
     st.subheader(f"Prediction: {predicted_class}")
     st.write(f"Confidence: {confidence:.4f}")
 
-    # Show probabilities
+    # Show probability chart
     st.subheader("Class Probabilities")
     st.bar_chart(prediction[0])
